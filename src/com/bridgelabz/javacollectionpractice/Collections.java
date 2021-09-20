a0c31be1c832e3d4c2bf20796e5b31e6595f32f1
sushant2931 <sushantlad8888@gmail.com>
2021-09-20 23:31:35 +0530

    Added : Welcome On Collection Program Practice

---
+++ src/com/bridgelabz/javacollectionpractice/Collections.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.javacollectionpractice;
+public class Collections {
+    public static void main(String[] args) {
+        System.out.println("...... Welcome On Collection Program Practice.....");
+    }
+}
